81c476f71e3fb55e7d33e91e6fc95bd6c4912ac6
witmaos6 <geonhee518@naver.com>
2021-12-03 20:06:40 +0900

    appjs수정

--- src/App.js
+++ src/App.js
@@ -1,11 +1,20 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import KakaoMapScript from './kakaomap';
 
 function App() {
-  return (
-    <div className="App">
-      <h1> hi</h1>
-    </div>
-  );
+    // useEffect 로 스크립트를 불러옴
+    useEffect(() => {
+        KakaoMapScript();
+    });
+    return (
+        <div
+            id="myMap"
+            style={{
+                width: '100vw',
+                height: '100vh',
+            }}
+        ></div>
+    );
 }
 
 export default App;
