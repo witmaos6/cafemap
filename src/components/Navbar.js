a6f6a04dd77d8568f5caf3a6c39fc435126d6b08
김진웅 <0802dragon@naver.com>
2021-12-05 18:00:08 +0900

    카페 사이드바 사진 추가

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -13,10 +13,8 @@ function Navbar() {
       <IconContext.Provider value={{ color: "#fff" }}>
         <nav className={sidebar ? "nav-menu active" : "nav-menu"}>
           <ul className="nav-menu-items">
-            카페 탐색기 
-            <hr></hr>
             <div>
-              카페 탐색기라니 너무행
+              카페 탐색기
             </div>
             {/* SidebarData를 순서대로 담기
             {Sidebardata.map((item, index) => {
