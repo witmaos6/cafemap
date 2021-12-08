c8c099cec21cbd55e191ab3c9230fe9fd20f42ef
김진웅 <0802dragon@naver.com>
2021-12-08 15:36:09 +0900

    에러가 드롭다운 메뉴와 드롭다운 메뉴의 css 파일, 상권형이 도와주기로 하여 푸시함

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -7,22 +7,58 @@ import { IconContext } from "react-icons";
 function Navbar() {
   const [sidebar, setSidebar] = useState(false);
   const showSidebar = () => setSidebar(!sidebar);
+  const MenuDrop = (e) => {
+    {
+      const isActive = e.currentTarget.className.indexOf("active") !== -1;
+      if (isActive) {
+        e.currentTarget.className = "select";
+      } else {
+        e.currentTarget.className = "select active";
+      }
+    }
+
+    document.querySelector("#theme .select").addEventListener("click", MenuDrop);
+  }
+  const ValueSelect = (e) => {
+    const selectedValue = e.currentTarget.innerHTML;
+    document.querySelector("#theme .text").innerHTML = selectedValue;
+
+    var optionList = document.querySelectorAll("#theme .option");
+    for (var i = 0; i < optionList.length; i++) {
+      var option = optionList[i];
+      option.addEventListener("click", ValueSelect);}
+  }
   return (
     <>
       {/* 아이콘 컬러 전체 변경 기능 */}
       <IconContext.Provider value={{ color: "#fff" }}>
         <nav className={sidebar ? "nav-menu active" : "nav-menu"}>
           <ul className="nav-menu-items">
-            
-            <div>
-              카페- 탐색기-
-            </div>
+            <body>
+              <div>
+                카페- 탐색기-
+              </div>
 
-            <div>
-              <input />
-              <button>목적지 탐색</button>
-            </div>
-         
+              <div>
+                <input />
+                <button>목적지 탐색</button>
+              </div>
+              <section id="theme">
+                <div class="select" onClick={ValueSelect}>
+                  <div class="text" onClick={MenuDrop}>연령대 선택</div>
+                  <ul class="option-list">
+                    <li class="option">9세 이하</li>
+                    <li class="option">10대</li>
+                    <li class="option">20대</li>
+                    <li class="option">30대</li>
+                    <li class="option">40대</li>
+                    <li class="option">50대</li>
+                    <li class="option">60대 이상</li>
+                  </ul>
+                </div>
+              </section>
+            </body>
+
             {/* SidebarData를 순서대로 담기
             {Sidebardata.map((item, index) => {
               return (
